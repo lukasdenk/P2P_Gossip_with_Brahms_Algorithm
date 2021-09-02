7b12f3ef5d91bc389805ced7668de337c47061c9
Kyrylo Vasylenko <kyryll.vasylenko@tum.de>
2021-09-02 14:32:51 +0200

    Add OneWayMessageClient who does not care for an answer

--- src/main/kotlin/networking/client/ClientMain.kt
+++ src/main/kotlin/networking/client/ClientMain.kt
@@ -1,37 +1,31 @@
 package networking.client
 
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.ExperimentalSerializationApi
 import messaging.api.GossipAnnounce
 import utils.MessageParser
 import utils.ParametersReader
 import java.nio.ByteBuffer
 import kotlin.time.ExperimentalTime
 
+@ExperimentalSerializationApi
 @ExperimentalTime
 fun main(args: Array<String>) {
     runBlocking {
         val parametersReader = ParametersReader()
         parametersReader.read(args)
-        // TODO create send p2p message that doesn't care for an answer
-        val client = Client(
-            gossipAddress = parametersReader.gossipServiceAddress,
-            gossipPort = parametersReader.gossipServicePort,
-            firstWrite = { writer ->
+        val oneWayMessageClient = OneWayMessageClient(
+            address = parametersReader.gossipServiceAddress,
+            port = parametersReader.gossipServicePort,
+            write = { writer ->
                 val message = GossipAnnounce(
                     timeToLive = 10,
                     dataType = 1,
                     data = byteArrayOf(1, 2, 3)
                 )
                 writer.invoke(message.toByteArray())
-            },
-            read = { buffer: ByteBuffer, writer: (ByteArray) -> Unit ->
-                println("Read message type: ${MessageParser().toApiMessage(buffer).javaClass.name}")
-                if (buffer.capacity() > 0) {
-                    // TODO put your custom action
-                    writer.invoke("qwerty".toByteArray())
-                }
             }
         )
-        client.start()
+        oneWayMessageClient.start()
     }
 }
---
+++ src/main/kotlin/networking/client/OneWayMessageClient.kt
@@ -0,0 +1,137 @@
+package networking.client
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import networking.service.Constants
+import java.net.InetSocketAddress
+import java.net.SocketAddress
+import java.nio.ByteBuffer
+import java.nio.channels.AsynchronousSocketChannel
+import java.nio.channels.CompletionHandler
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
+
+/**
+ * @param write Writes first message as soon as connection established
+ */
+@ExperimentalTime
+@Suppress("BlockingMethodInNonBlockingContext")
+class OneWayMessageClient(
+    address: String,
+    port: Int,
+    private val write: ((ByteArray) -> Unit) -> Unit
+) {
+
+    private val socketScope  = CoroutineScope(Dispatchers.IO)
+    private val address: SocketAddress = InetSocketAddress(address, port)
+    private val reconnectedTimes = AtomicInteger(0)
+    private val isUp = AtomicBoolean(true)
+
+    suspend fun start() {
+        connect()
+        while (isUp.get()) {
+            delay(Duration.seconds(10))
+        }
+    }
+
+    private fun connect() {
+        isUp.set(true)
+        socketScope.launch {
+            val socketChannel = AsynchronousSocketChannel.open()
+            socketChannel.connect(address, socketChannel, ConnectionHandler(
+                write,
+                connectionClosed = { println("closed the connection") },
+                connectionFailed = { reconnect() }
+            ))
+        }
+    }
+
+    private fun reconnect() {
+        if (reconnectedTimes.addAndGet(1) > Constants.MaxReconnectAttempts) {
+            println("[${this::class.simpleName}] Was not able to connect to the gossip service.")
+            isUp.set(false)
+            return
+        }
+        println("[${this::class.simpleName}] Attempt #${reconnectedTimes.get()} to reconnect after ${Constants.ReconnectionIntervalInSec} sec")
+        socketScope.launch {
+            delay(Duration.seconds(Constants.ReconnectionIntervalInSec))
+            connect()
+        }
+    }
+
+    private class ConnectionHandler(
+        private val write: ((ByteArray) -> Unit) -> Unit,
+        private val connectionClosed: () -> Unit = {},
+        private val connectionFailed: () -> Unit = {}
+    ) : CompletionHandler<Void, AsynchronousSocketChannel> {
+
+        private lateinit var socketChannel: AsynchronousSocketChannel
+
+        override fun completed(result: Void?, socketChannel: AsynchronousSocketChannel) {
+            this.socketChannel = socketChannel
+            println("[${this::class.simpleName}] Connected to ${socketChannel.remoteAddress}")
+            sendFirstMessage()
+        }
+
+        private fun sendFirstMessage() {
+            if (!socketChannel.isOpen) {
+                closeChannel()
+                return
+            }
+            write.invoke { bytes: ByteArray ->
+                write(bytes)
+            }
+        }
+
+        private fun write(bytes: ByteArray) {
+            socketChannel.write(
+                ByteBuffer.wrap(bytes),
+                bytes,
+                WriteHandler(
+                    writeCompleted = {
+                        closeChannel()
+                    }
+                )
+            )
+        }
+
+        private fun closeChannel() {
+            socketChannel.close()
+            connectionClosed.invoke()
+        }
+
+        override fun failed(exc: Throwable?, socketChannel: AsynchronousSocketChannel) {
+            println("[${this::class.simpleName}] failed to connect $exc")
+            connectionFailed.invoke()
+        }
+
+    }
+
+    private class WriteHandler(
+        private val writeCompleted: () -> Unit = {},
+        private val writeFailed: () -> Unit = {}
+    ): CompletionHandler<Int, ByteArray> {
+
+        override fun completed(result: Int, attachment: ByteArray) {
+            log(attachment)
+            writeCompleted.invoke()
+        }
+
+        private fun log(bytes: ByteArray) {
+            println(
+                "[${this::class.simpleName}] sent " +
+                        bytes.map(Byte::toInt).joinToString(separator = " ") { String.format("%02X", it) }
+            )
+        }
+
+        override fun failed(exc: Throwable, attachment: ByteArray) {
+            println("[${this::class.simpleName}] write failed")
+            writeFailed.invoke()
+        }
+
+    }
+}
