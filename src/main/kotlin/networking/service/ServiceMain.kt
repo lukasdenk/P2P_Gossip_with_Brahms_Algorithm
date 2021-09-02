98d2a61e5b90dc1b50d25943b8c716c467dce0a2
Kyrylo Vasylenko <kyryll.vasylenko@tum.de>
2021-09-02 12:16:00 +0200

    Minor code changes

--- src/main/kotlin/networking/service/ServiceMain.kt
+++ src/main/kotlin/networking/service/ServiceMain.kt
@@ -1,11 +1,14 @@
 package networking.service
 
+import api.manager.APIMessagesManager
 import kotlinx.coroutines.runBlocking
 import utils.MessageParser
 import utils.ParametersReader
 import java.nio.ByteBuffer
 import kotlin.time.ExperimentalTime
 
+// TODO combine two main modules
+// TODO
 @ExperimentalTime
 fun main(args: Array<String>) {
     runBlocking {
@@ -16,6 +19,10 @@ fun main(args: Array<String>) {
             address = propertiesReader.serviceAddress,
             port = propertiesReader.servicePort,
             read = { data: ByteBuffer, writer: (ByteArray) -> Unit ->
+                // TODO check if it is P2P or API message
+                //  If this is P2P we call
+                //  If this is API we call
+                APIMessagesManager.receive(MessageParser().toApiMessage(data), 8080)// TODO pass the port to the result
                 println("Received message of type: ${MessageParser().toApiMessage(data).javaClass.name}")
                 if (data.capacity() > 0) {
                     // TODO to write something in response use writer
