6994bef6a32a4d9c839c8e3aaea91eece1e33607
Lukas Denk <lukasdenk@web.de>
2021-09-02 12:14:35 +0200

    P2PMessagesManager.kt

---
+++ src/main/kotlin/p2p/brahms/P2PMessagesManager.kt
@@ -0,0 +1,12 @@
+package p2p.brahms
+
+import messaging.p2p.P2PMessage
+import messaging.p2p.P2PMessageListener
+import messaging.p2p.Peer
+
+class P2PMessagesManager : P2PMessageListener {
+    override fun receive(msg: P2PMessage, sender: Peer) {
+        TODO("Not yet implemented")
+    }
+
+}
