112dd15a611db2ff29ebbba6a359c46461d5245e
Lukas Denk <lukasdenk@web.de>
2021-09-03 20:13:53 +0200

    fixes

--- src/main/kotlin/main/Extensions.kt
+++ src/main/kotlin/main/Extensions.kt
@@ -13,9 +13,10 @@ operator fun ByteArray.compareTo(other: ByteArray?): Int {
     if (other == null) {
         return 1
     }
-    val lengthComparison = this.size.compareTo(other.size)
-    if (lengthComparison != 0) {
-        return lengthComparison
+
+    val leadingZeroesComparison = other.leadingZeroes().compareTo(this.leadingZeroes())
+    if (leadingZeroesComparison != 0) {
+        return leadingZeroesComparison
     }
     for (i in 0..this.size) {
         val comp = this[i].compareTo(other[i])
@@ -23,20 +24,25 @@ operator fun ByteArray.compareTo(other: ByteArray?): Int {
             return comp
         }
     }
-    return 1
+    return 0
 }
 
 fun ByteArray.startsWithXLeadingZeroes(x: Int): Boolean {
-    if (this.size < x) {
-        return false
-    }
-    val zero: Byte = 0
-    for (i in 0..x) {
-        if (this[i] != zero) {
-            return false
+    return this.leadingZeroes() >= x
+}
+
+fun ByteArray.leadingZeroes(): Int {
+    var leadingZeroes = 0
+    for (i in 0..this.size) {
+        val firstOneBit = Integer.toBinaryString(this[i].toInt()).indexOf('1')
+        if (firstOneBit == -1) {
+            leadingZeroes += 8
+        } else {
+            leadingZeroes += firstOneBit
+            return leadingZeroes
         }
     }
-    return true
+    return leadingZeroes
 }
 
 fun <T> Collection<T>.randomSubSet(n: Int): MutableSet<T> {
