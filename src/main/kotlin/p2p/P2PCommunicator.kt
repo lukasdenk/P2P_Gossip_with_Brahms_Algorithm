198a56f451bdedfd2f906483c0a3a078e9bbea95
Lukas Denk <lukasdenk@web.de>
2021-09-04 14:49:41 +0200

    rmv TODOs

--- src/main/kotlin/p2p/P2PCommunicator.kt
+++ src/main/kotlin/p2p/P2PCommunicator.kt
@@ -9,14 +9,11 @@ import p2p.brahms.manager.PullManager
 import p2p.brahms.manager.PushManager
 import kotlin.time.ExperimentalTime
 
-//TODO To be done by kyrylo
 
 @ExperimentalTime
 object P2PCommunicator : P2PMessageListener {
-    //    TODO: call listener's receive()-fun for incoming messages
     val listeners: List<P2PMessageListener> = listOf(PullManager, PushManager)
 
-    //    TODO: send in launch (best would be in networking module)
     fun send(msg: P2PMessage, receiver: Peer) {
         ServicesManager.sendP2PMessage(msg, receiver)
         println("send ${JsonMapper.mapToJsonString(msg)} to ${receiver.port}")
