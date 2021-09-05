77abe212215f86d8a3035b8ee196675bed0efba1
Kyrylo Vasylenko <kyryll.vasylenko@tum.de>
2021-09-05 13:57:18 +0200

    Update mac related scripts

--- deployment/mac/build.sh
+++ deployment/mac/build.sh
@@ -1,8 +1,9 @@
+./gradlew clean
+rm -f deployment/mac/Gossip-1.0-SNAPSHOT-all.jar
+rm -f deployment/mac/service.ini
+rm -f -R deployment/mac/test_inis
 ./gradlew build
 ./gradlew shadowJar
-rm deployment/mac/Gossip-1.0-SNAPSHOT-all.jar
-rm deployment/mac/service.ini
-rm -R deployment/mac/test_inis
 cp build/libs/Gossip-1.0-SNAPSHOT-all.jar deployment/mac/
 cp -R build/resources/main/test_inis deployment/mac/
 cp build/resources/main/service.ini deployment/mac/
--- deployment/mac/clean.sh
+++ deployment/mac/clean.sh
@@ -1,4 +1,4 @@
 ./gradlew clean
-rm deployment/mac/Gossip-1.0-SNAPSHOT-all.jar
-rm deployment/mac/service.ini
-rm -R deployment/mac/test_inis
+rm -f deployment/mac/Gossip-1.0-SNAPSHOT-all.jar
+rm -f deployment/mac/service.ini
+rm -f -R deployment/mac/test_inis
