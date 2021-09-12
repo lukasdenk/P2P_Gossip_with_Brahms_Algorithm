830887bdf2c1e48b8d0eb7fa0d63b4a276156da7
Kyryll Vasylenko <vasilenkowm@gmail.com>
2021-09-12 16:36:35 +0200

    Fix cmd build script

--- deployment/windows/build_and_run.cmd
+++ deployment/windows/build_and_run.cmd
@@ -1,6 +1,6 @@
 if exist deployment\windows\Gossip-1.0-SNAPSHOT-all.jar del deployment\windows\Gossip-1.0-SNAPSHOT-all.jar
 if exist deployment\windows\service.ini del deployment\windows\service.ini
-if exist deployment\windows\test_inis rmdir /f /s deployment\windows\test_inis
+if exist deployment\windows\test_inis rmdir /q /s deployment\windows\test_inis
 gradlew clean build shadowJar && ^
 copy build\libs\Gossip-1.0-SNAPSHOT-all.jar deployment\windows\ && ^
 copy build\resources\main\service.ini deployment\windows\ && ^
--- deployment/windows/clean.cmd
+++ deployment/windows/clean.cmd
@@ -1,4 +1,4 @@
 if exist deployment\windows\Gossip-1.0-SNAPSHOT-all.jar del deployment\windows\Gossip-1.0-SNAPSHOT-all.jar
 if exist deployment\windows\service.ini del deployment\windows\service.ini
-if exist deployment\windows\test_inis\ rmdir /f /s deployment\windows\test_inis\
+if exist deployment\windows\test_inis\ rmdir /q /s deployment\windows\test_inis\
 gradlew clean
